eeaac2a01775b9d740935b96f5843de54b694f20
MukkiiiNishu <22.sharma.mukesh.22@gmail.com>
2025-05-03 05:51:42 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      animation: {
+        fadeIn: "fadeIn 1s ease-in-out forwards",
+        ping: "ping 1s cubic-bezier(0, 0, 0.2, 1) infinite",
+        bounce: "bounce 1.5s infinite",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
